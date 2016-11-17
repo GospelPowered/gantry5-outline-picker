346e70a5205bf5ee3294fe609b03d92351f09172
Nikola Miljkovic <nikolam3244@gmail.com>
2016-11-17 17:53:07 +0100

    Initial commit

---
+++ gantry5-outline-picker.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Plugin Name: Gantry 5 Outline Picker
+ * Plugin URI: http://gospelpowered.com/
+ * Description: Outline picker for WordPres pages on Gantry 5 based themes.
+ * Version: 1.0.0
+ * Author: Gospel Powered
+ * Author URI: http://gospelpowered.com/
+ * License: GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: gantry5-outline-picker
+ */
+
+/*
+ * Load class if it doesn't exist
+ */
+spl_autoload_register(function ($class) {
+	$prefix = 'GOP';
+	$base_dir = __DIR__;
+	$len = strlen($prefix);
+	if (strncmp($prefix, $class, $len) !== 0) {
+		return;
+	}
+	$relative_class = substr($class, $len);
+	$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+	if (file_exists($file)) {
+		/** @noinspection PhpIncludeInspection */
+		require $file;
+	}
+} );
+
+/**
+ * Create meta box
+ * 
+ * @since 1.0.0
+ */
+function gop_register_meta_box() {
+    add_meta_box( 'gop-picker', __( 'Outline Assignment', 'gantry5-outline-picker' ), 'gop_init', 'page', 'side', 'default' );
+}
+add_action( 'add_meta_boxes', 'gop_register_meta_box' );
+
+/**
+ * Update assignments on page save
+ * 
+ * @since 1.0.0
+ * @param int $post_id Page ID
+ */
+function gop_save_post_init( $post_id ) {
+	if ( isset( $_POST[ 'gop_outline' ] ) ) {
+		$gop_save = new GOP\gop\gop_save;
+		$gop_save->save_assignments( $post_id, sanitize_text_field( $_POST[ 'gop_outline' ] ) );
+	}
+}
+add_action( 'save_post', 'gop_save_post_init' );
+
+/**
+ * Create new instance of main class
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function gop_init(){
+	return new GOP\gop\gop_read;
+}
+
+/**
+ * Add our custom CSS
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function gop_enqueue_scripts() {
+	if ( $GLOBALS['pagenow'] === 'post.php' && $_GET[ 'action' ] === 'edit' && get_post_type() === 'page' ) {
+		wp_enqueue_style( 'gop-main', plugin_dir_url( __FILE__ ) . 'assets/gop.css' );
+	}
+}
+add_action( 'admin_enqueue_scripts', 'gop_enqueue_scripts' );
---
+++ gop/gop_save.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace GOP\gop;
+use GOP\vendor\Spyc\Spyc;
+
+class gop_save extends gop_read {
+
+	/**
+	 * Function used to update outline assignments
+	 *
+	 * @since 1.0.0
+	 * @param int $page_id ID of the page
+	 * @param string $outline_slug Name of the outline folder
+	 * @return bool True on success
+	 */
+	function save_assignments( $page_id, $outline_slug ){
+		$outline_dir      = $this->outlines_dir . '/' . $outline_slug;
+		$assignments_file = $outline_dir . '/assignments.yaml';
+		
+		if ( !file_exists( $assignments_file ) ){
+			touch( $assignments_file );
+		}
+		
+		$this->clear_assignments( $page_id );
+		
+		$assignments = Spyc::YAMLLoad( $assignments_file );
+		
+		$assignments[ 'post' ][ 'page' ][ $page_id ] = true;
+		
+		file_put_contents( $assignments_file, Spyc::YAMLDump( $assignments ), LOCK_EX );
+		
+		return true;
+	}
+	
+	/**
+	 * Function used to remove all assignments for a page
+	 *
+	 * @since 1.0.0
+	 * @param int $page_id
+	 * @return void
+	 */
+	function clear_assignments( $page_id ) {
+		foreach( $this->outlines as $slug => $outline ) {
+			$assignments_file = $this->outlines_dir . '/' . $slug . '/assignments.yaml';
+			
+			if ( file_exists( $assignments_file ) ) {
+				$assignments = Spyc::YAMLLoad( $assignments_file );
+				
+				if ( isset( $assignments[ 'post' ][ 'page' ][ $page_id ] ) ) {
+					$assignments[ 'post' ][ 'page' ][ $page_id ] = false;
+					file_put_contents( $assignments_file, Spyc::YAMLDump( $assignments ), LOCK_EX );
+				}
+			}
+		}
+	}
+}
