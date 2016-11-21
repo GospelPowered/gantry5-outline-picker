ae681389f6d0690c8795dab6e71015c7e5f9204d
Nikola Miljkovic <nikolam3244@gmail.com>
2016-11-21 13:18:03 +0100

    Reformat code

--- gantry5-outline-picker.php
+++ gantry5-outline-picker.php
@@ -14,16 +14,16 @@
 /*
  * Load class if it doesn't exist
  */
-spl_autoload_register(function ($class) {
-	$prefix = 'GOP';
+spl_autoload_register( function ( $class ) {
+	$prefix   = 'GOP';
 	$base_dir = __DIR__;
-	$len = strlen($prefix);
-	if (strncmp($prefix, $class, $len) !== 0) {
+	$len      = strlen( $prefix );
+	if ( strncmp( $prefix, $class, $len ) !== 0 ) {
 		return;
 	}
-	$relative_class = substr($class, $len);
-	$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-	if (file_exists($file)) {
+	$relative_class = substr( $class, $len );
+	$file           = $base_dir . str_replace( '\\', '/', $relative_class ) . '.php';
+	if ( file_exists( $file ) ) {
 		/** @noinspection PhpIncludeInspection */
 		require $file;
 	}
@@ -31,26 +31,29 @@ spl_autoload_register(function ($class) {
 
 /**
  * Create meta box
- * 
+ *
  * @since 1.0.0
  */
 function gop_register_meta_box() {
-    add_meta_box( 'gop-picker', __( 'Outline Assignment', 'gantry5-outline-picker' ), 'gop_init', 'page', 'side', 'default' );
+	add_meta_box( 'gop-picker', __( 'Outline Assignment', 'gantry5-outline-picker' ), 'gop_init', 'page', 'side', 'default' );
 }
+
 add_action( 'add_meta_boxes', 'gop_register_meta_box' );
 
 /**
  * Update assignments on page save
- * 
+ *
  * @since 1.0.0
+ *
  * @param int $post_id Page ID
  */
 function gop_save_post_init( $post_id ) {
-	if ( isset( $_POST[ 'gop_outline' ] ) ) {
+	if ( isset( $_POST['gop_outline'] ) ) {
 		$gop_save = new GOP\gop\gop_save;
-		$gop_save->save_assignments( $post_id, sanitize_text_field( $_POST[ 'gop_outline' ] ) );
+		$gop_save->save_assignments( $post_id, sanitize_text_field( $_POST['gop_outline'] ) );
 	}
 }
+
 add_action( 'save_post', 'gop_save_post_init' );
 
 /**
@@ -59,7 +62,7 @@ add_action( 'save_post', 'gop_save_post_init' );
  * @since 1.0.0
  * @return void
  */
-function gop_init(){
+function gop_init() {
 	new GOP\gop\gop_read;
 }
 
@@ -70,12 +73,13 @@ function gop_init(){
  * @return void
  */
 function gop_enqueue_scripts() {
-	if ( $GLOBALS['pagenow'] === 'post.php' && $_GET[ 'action' ] === 'edit' && get_post_type() === 'page' ) {
+	if ( $GLOBALS['pagenow'] === 'post.php' && $_GET['action'] === 'edit' && get_post_type() === 'page' ) {
 		/** @noinspection PhpIncludeInspection */
 		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-		$plugin_path = is_plugin_active( substr( __FILE__, strrpos( __FILE__, '/', -28 ) + 1 ) ) ? plugin_dir_url( __FILE__ ) : get_template_directory_uri() . '/includes/vendor/gantry5-outline-picker/';
+		$plugin_path = is_plugin_active( substr( __FILE__, strrpos( __FILE__, '/', - 28 ) + 1 ) ) ? plugin_dir_url( __FILE__ ) : get_template_directory_uri() . '/includes/vendor/gantry5-outline-picker/';
 
 		wp_enqueue_style( 'gop-main', $plugin_path . 'assets/gop.css' );
 	}
 }
+
 add_action( 'admin_enqueue_scripts', 'gop_enqueue_scripts' );
--- gop/gop_read.php
+++ gop/gop_read.php
@@ -1,46 +1,48 @@
 <?php
 
 namespace GOP\gop;
+
 use GOP\vendor\Spyc\Spyc;
 
 class gop_read {
-	
+
 	/**
 	 * @var string Outlines location, usually at theme/custom/config/
 	 * @since 1.0.0
 	 */
-	
+
 	public $outlines_dir;
-	
+
 	/**
 	 * @var string Error message
 	 * @since 1.0.0
 	 */
-	
+
 	public $error_message = 'An error has occured.';
-	
+
 	/**
 	 * @var array Array of outlines
 	 * @since 1.0.0
 	 */
-	
+
 	public $outlines = array(
 		'default' => 'Base Outline',
 	);
-	
-	function __construct(){
+
+	function __construct() {
 		$this->outlines_dir = get_template_directory() . '/custom/config';
-		
-		if ( !$this->pre_init_tests() || !$this->get_outlines() ) {
+
+		if ( ! $this->pre_init_tests() || ! $this->get_outlines() ) {
 			echo $this->spawn_error( $this->error_message );
+
 			return;
 		}
-		
-		if ( isset( $_GET[ 'action' ] ) && $_GET[ 'action' ] === 'edit' ) {
+
+		if ( isset( $_GET['action'] ) && $_GET['action'] === 'edit' ) {
 			echo $this->show_outlines();
 		}
 	}
-	
+
 	/**
 	 * Check if everything is good and proceed
 	 *
@@ -48,25 +50,56 @@ class gop_read {
 	 * @return bool Good or not
 	 */
 	function pre_init_tests() {
-		if ( !is_dir( $this->outlines_dir ) ) {
+		if ( ! is_dir( $this->outlines_dir ) ) {
 			$this->error_message = 'Outlines do not exist!';
+
 			return false;
 		}
-		
+
 		return true;
-	} 
-	
+	}
+
+	/**
+	 * Get a list of outlines
+	 *
+	 * @since 1.0.0
+	 * @return bool true on success, false otherwise
+	 */
+	function get_outlines() {
+		$outlines_list = array_slice( scandir( $this->outlines_dir ), 2 );
+		$outlines      = array();
+
+		if ( empty( $outlines_list ) ) {
+			$this->error_message = 'No outlines found.';
+
+			return false;
+		}
+
+		// Remove system and default outlines
+		foreach ( $outlines_list as $key => $outline ) {
+			if ( substr( $outline, 0, 1 ) !== '_' && $outline !== 'default' ) {
+				$outlines[ $outline ] = ucwords( str_replace( '_', ' ', $outline ) );
+			}
+		}
+
+		$this->outlines += $outlines;
+
+		return true;
+	}
+
 	/**
 	 * Show a formatted error message
 	 *
 	 * @since 1.0.0
+	 *
 	 * @param string $message The message to format
+	 *
 	 * @return string
 	 */
 	function spawn_error( $message ) {
 		return $message;
 	}
-	
+
 	/**
 	 * Show a list of outlines
 	 *
@@ -75,67 +108,40 @@ class gop_read {
 	 */
 	function show_outlines() {
 		$assigned_outline = $this->get_assigned_outline();
-		
+
 		$return = '<select name="gop_outline">';
-		
-		foreach( $this->outlines as $slug => $outline ){
+
+		foreach ( $this->outlines as $slug => $outline ) {
 			$return .= '<option value="' . $slug . '" ' . ( $assigned_outline === $slug ? 'selected' : ( $slug === 'default' ? 'selected' : '' ) ) . '>' . $outline . '</option>';
 		}
-		
+
 		$return .= '</select>';
 
 		return $return;
 	}
-	
+
 	/**
 	 * Return assigned outline if there is one
-	 * 
+	 *
 	 * @since 1.0.0
 	 * @return bool|string false if there isn't one, slug if there is
 	 */
 	function get_assigned_outline() {
-		$current_post = $_GET[ 'post' ];
+		$current_post = $_GET['post'];
 		settype( $current_post, 'int' );
-		
-		foreach ( $this->outlines as $slug => $outline ){
+
+		foreach ( $this->outlines as $slug => $outline ) {
 			$assignments_file = $this->outlines_dir . '/' . $slug . '/assignments.yaml';
-			
+
 			if ( file_exists( $assignments_file ) ) {
 				$assignments = Spyc::YAMLLoad( $assignments_file );
-			
-				if ( isset( $assignments[ 'post' ][ 'page' ][ $current_post ] ) && $assignments[ 'post' ][ 'page' ][ $current_post ] === true ) {
+
+				if ( isset( $assignments['post']['page'][ $current_post ] ) && $assignments['post']['page'][ $current_post ] === true ) {
 					return $slug;
 				}
 			}
 		}
-		
+
 		return false;
-	}
-	
-	/**
-	 * Get a list of outlines
-	 *
-	 * @since 1.0.0
-	 * @return bool true on success, false otherwise
-	 */
-	function get_outlines(){
-		$outlines_list = array_slice( scandir( $this->outlines_dir ), 2 );
-		$outlines = array();
-		
-		if ( empty( $outlines_list ) ) {
-			$this->error_message = 'No outlines found.';
-			return false;
-		}
-		
-		// Remove system and default outlines
-		foreach ( $outlines_list as $key => $outline ) {
-			if ( substr( $outline, 0, 1 ) !== '_' && $outline !== 'default' ){
-				$outlines[ $outline ] = ucwords( str_replace( '_', ' ', $outline ) );
-			}
-		}
-		
-		$this->outlines += $outlines;
-		
-		return true;
 	}
 }
