eb8acfdccb65cf3c0c298c9883a8f79d65977897
Nikola <nikolam3244@gmail.com>
2017-09-06 16:32:24 +0200

    Fix stylesheets path

--- gantry5-outline-picker.php
+++ gantry5-outline-picker.php
@@ -76,7 +76,7 @@ function gop_enqueue_scripts() {
 	if ( ( ( $GLOBALS['pagenow'] === 'post.php' && $_GET['action'] === 'edit' ) || $GLOBALS['pagenow'] === 'post-new.php' ) && get_post_type() === 'page' ) {
 		/** @noinspection PhpIncludeInspection */
 		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-		$plugin_path = is_plugin_active( substr( __FILE__, strrpos( __FILE__, '/', - 28 ) + 1 ) ) ? plugin_dir_url( __FILE__ ) : get_template_directory_uri() . '/includes/vendor/gantry5-outline-picker/';
+		$plugin_path = is_plugin_active( substr( __FILE__, strrpos( __FILE__, '/', - 28 ) + 1 ) ) ? plugin_dir_url( __FILE__ ) : get_template_directory_uri() . '/includes/gantry5-outline-picker/';
 
 		wp_enqueue_style( 'gop-main', $plugin_path . 'assets/gop.css' );
 	}
